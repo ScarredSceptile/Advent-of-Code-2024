dbb3b9ebabf961292503e865e97c2e175ec58584
jonas <jonas.haegd@gmail.com>
2024-12-13 19:33:17 +0100

    Remove unrequired code

--- Days/Day13.cs
+++ Days/Day13.cs
@@ -1,46 +1,39 @@
-﻿namespace Advent_of_Coding_2024.Days
+﻿using System.Reflection.PortableExecutable;
+
+namespace Advent_of_Coding_2024.Days
 {
     internal class Day13 : Day
     {
         public void Star1()
         {
-            var machines = GetMachines();
-            Console.WriteLine(machines.Sum(TokensToWin));
+            Solve();
         }
 
         public void Star2()
         {
-            var machines = GetMachines(10000000000000);
-            Console.WriteLine(machines.Sum(TokensToWin));
+            Solve(10000000000000);
         }
 
-        private List<((long, long), (long, long), (long, long))> GetMachines(long addition = 0)
+        private void Solve(long addition = 0)
         {
             var input = Input.GetSingle("Day13").Split("\r\n\r\n");
-            List<((long, long), (long, long), (long, long))> machines = new();
+            long total = 0;
             foreach (var block in input)
             {
                 var lines = block.Split("\r\n");
                 var aVals = lines[0].Split(": ")[1].Split(", ");
                 var bVals = lines[1].Split(": ")[1].Split(", ");
                 var priceVals = lines[2].Split(": ")[1].Split(", ");
-                var a = (long.Parse(aVals[0].Split('+')[1]), long.Parse(aVals[1].Split('+')[1]));
-                var b = (long.Parse(bVals[0].Split('+')[1]), long.Parse(bVals[1].Split('+')[1]));
-                var price = (long.Parse(priceVals[0].Split('=')[1]) + addition, long.Parse(priceVals[1].Split('=')[1]) + addition);
-                machines.Add((a, b, price));
-            }
-            return machines;
-        }
+                (long X, long Y) a = (long.Parse(aVals[0].Split('+')[1]), long.Parse(aVals[1].Split('+')[1]));
+                (long X, long Y) b = (long.Parse(bVals[0].Split('+')[1]), long.Parse(bVals[1].Split('+')[1]));
+                (long X, long Y) price = (long.Parse(priceVals[0].Split('=')[1]) + addition , long.Parse(priceVals[1].Split('=')[1]) + addition );
 
-        private long TokensToWin(((long X, long Y) A, (long X, long Y) B, (long X, long Y) Price) machine)
-        {
-            if (machine.A.X * machine.B.Y - machine.B.X * machine.A.Y == 0)
-                return 0;
-            var x = (machine.Price.X * machine.B.Y - machine.B.X * machine.Price.Y)/(double)(machine.A.X * machine.B.Y - machine.B.X * machine.A.Y);
-            var y = (machine.A.X * machine.Price.Y - machine.Price.X * machine.A.Y)/(double)(machine.A.X * machine.B.Y - machine.B.X * machine.A.Y);
-            if (Math.Abs(x-(long)x) < double.Epsilon && Math.Abs(y-(long)y) < double.Epsilon)
-                return (long)x * 3 + (long)y;
-            return 0;
+                var x = (price.X * b.Y - b.X * price.Y)/(double)(a.X * b.Y - b.X * a.Y);
+                var y = (a.X * price.Y - price.X * a.Y)/(double)(a.X * b.Y - b.X * a.Y);
+                if (Math.Abs(x-(long)x) < double.Epsilon && Math.Abs(y-(long)y) < double.Epsilon)
+                    total += (long)x * 3 + (long)y;
+            }
+            Console.WriteLine(total);
         }
     }
 }
